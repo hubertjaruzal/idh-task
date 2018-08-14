f71dd35dfe7bfd18de5063d3ac89638e819e5777
hubertjaruzal <hubertjaruzal@gmail.com>
2018-08-14 16:45:50 +0200

    init commit

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <section className="app">
+
+      </section>
+    );
+  }
+}
+
+export default App;
